066254b6c8510bf28189ba99d8d99c2c05dba0d3
Miroslav Lichvar <mlichvar@redhat.com>
2013-06-05 10:17:13 +0200

    Fix burst command with specified address
    This was broken in commit 0f8def4ca4237495f13a93384ded9245495e3c8f.

--- client.c
+++ client.c
@@ -589,17 +589,19 @@ process_cmd_burst(CMD_Request *msg, char *line)
   s2 = CPS_SplitWord(s1);
   CPS_SplitWord(s2);
 
-  if (sscanf(s1, "%d/%d", &n_good_samples, &n_total_samples) != 2 ||
-      (*s2 && !read_mask_address(s2, &mask, &address))) {
+  if (sscanf(s1, "%d/%d", &n_good_samples, &n_total_samples) != 2) {
     fprintf(stderr, "Invalid syntax for burst command\n");
+    return 0;
+  }
+
+  mask.family = address.family = IPADDR_UNSPEC;
+  if (*s2 && !read_mask_address(s2, &mask, &address)) {
     return 0;
   }
 
   msg->command = htons(REQ_BURST);
   msg->data.burst.n_good_samples = ntohl(n_good_samples);
   msg->data.burst.n_total_samples = ntohl(n_total_samples);
-
-  mask.family = address.family = IPADDR_UNSPEC;
 
   UTI_IPHostToNetwork(&mask, &msg->data.burst.mask);
   UTI_IPHostToNetwork(&address, &msg->data.burst.address);
