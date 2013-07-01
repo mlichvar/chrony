829b3adac3422e1605a121a2586267fee31dc901
Miroslav Lichvar <mlichvar@redhat.com>
2013-07-01 17:52:23 +0200

    Update copyright in chronyc GPL string

--- client.c
+++ client.c
@@ -2856,7 +2856,7 @@ static void
 display_gpl(void)
 {
     printf("chrony version %s\n"
-           "Copyright (C) 1997-2003, 2007, 2009-2012 Richard P. Curnow and others\n"
+           "Copyright (C) 1997-2003, 2007, 2009-2013 Richard P. Curnow and others\n"
            "chrony comes with ABSOLUTELY NO WARRANTY.  This is free software, and\n"
            "you are welcome to redistribute it under certain conditions.  See the\n"
            "GNU General Public License version 2 for details.\n\n",
