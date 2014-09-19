767a8b19a97c17aa7077d7d97c76c84e93154bc9
Miroslav Lichvar <mlichvar@redhat.com>
2014-09-19 11:06:37 +0200

    configure: unify macro naming for available headers

--- sysincl.h
+++ sysincl.h
@@ -62,9 +62,9 @@
 #include <syslog.h>
 #include <time.h>
 
-#ifdef HAS_INTTYPES_H
+#ifdef HAVE_INTTYPES_H
 #include <inttypes.h>
-#elif HAS_STDINT_H
+#elif HAVE_STDINT_H
 #include <stdint.h>
 #else
 /* Tough */
