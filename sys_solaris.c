mlichvar/chrony

commit b6a27df5b9be0f07f151c8fba311cb7eadb2b13e

sys: include predicted drift in adjtime() offset
In drivers with periodic drift removal include in the adjustment also a
prediction of the error gained in half of the interval to move the mean
offset of the clock closer to zero. E.g. offset of a stable clock
drifting by 10 ppm should now be closer to 0 +/- 5 microseconds instead
of 5 +/- 5 microseconds.

sys_solaris.c
@@ -127,7 +127,7 @@ start_adjust(void)
 {
   struct timeval newadj, oldadj;
   struct timeval T1;
-  double elapsed, accrued_error;
+  double elapsed, accrued_error, predicted_error;
   double adjust_required;
   struct timeval exact_newadj;
   double rounding_error;
@@ -140,8 +140,9 @@ start_adjust(void)
 
   UTI_DiffTimevalsToDouble(&elapsed, &T1, &T0);
   accrued_error = elapsed * current_freq;
+  predicted_error = DRIFT_REMOVAL_INTERVAL / 2.0 * current_freq;
   
-  adjust_required = - (accrued_error + offset_register);
+  adjust_required = - (accrued_error + offset_register + predicted_error);
 
   UTI_DoubleToTimeval(adjust_required, &exact_newadj);
 
@@ -160,7 +161,7 @@ start_adjust(void)
 
   UTI_TimevalToDouble(&oldadj, &old_adjust_remaining);
 
-  offset_register = rounding_error - old_adjust_remaining;
+  offset_register = rounding_error - old_adjust_remaining - predicted_error;
 
   T0 = T1;
   UTI_TimevalToDouble(&newadj, &adjustment_requested);

sys_sunos.c
@@ -118,7 +118,7 @@ start_adjust(void)
 {
   struct timeval newadj, oldadj;
   struct timeval T1;
-  double elapsed, accrued_error;
+  double elapsed, accrued_error, predicted_error;
   double adjust_required;
   struct timeval exact_newadj;
   double rounding_error;
@@ -132,8 +132,9 @@ start_adjust(void)
 
   UTI_DiffTimevalsToDouble(&elapsed, &T1, &T0);
   accrued_error = elapsed * current_freq;
+  predicted_error = DRIFT_REMOVAL_INTERVAL / 2.0 * current_freq;
   
-  adjust_required = - (accrued_error + offset_register);
+  adjust_required = - (accrued_error + offset_register + predicted_error);
 
   UTI_DoubleToTimeval(adjust_required, &exact_newadj);
 
@@ -163,7 +164,7 @@ start_adjust(void)
 
   UTI_TimevalToDouble(&oldadj, &old_adjust_remaining);
 
-  offset_register = rounding_error - old_adjust_remaining;
+  offset_register = rounding_error - old_adjust_remaining - predicted_error;
 
   T0 = T1;
   UTI_TimevalToDouble(&newadj, &adjustment_requested);

sys_macosx.c
@@ -108,7 +108,7 @@ start_adjust(void)
 {
   struct timeval newadj, oldadj;
   struct timeval T1;
-  double elapsed, accrued_error;
+  double elapsed, accrued_error, predicted_error;
   double adjust_required;
   double rounding_error;
   double old_adjust_remaining;
@@ -120,8 +120,9 @@ start_adjust(void)
 
   UTI_DiffTimevalsToDouble(&elapsed, &T1, &T0);
   accrued_error = elapsed * current_freq;
+  predicted_error = DRIFT_REMOVAL_INTERVAL / 2.0 * current_freq;
 
-  adjust_required = - (accrued_error + offset_register);
+  adjust_required = - (accrued_error + offset_register + predicted_error);
 
   UTI_DoubleToTimeval(adjust_required, &newadj);
   UTI_TimevalToDouble(&newadj, &adjustment_requested);
@@ -133,7 +134,7 @@ start_adjust(void)
 
   UTI_TimevalToDouble(&oldadj, &old_adjust_remaining);
 
-  offset_register = rounding_error - old_adjust_remaining;
+  offset_register = rounding_error - old_adjust_remaining - predicted_error;
 
   T0 = T1;
 }

commit 4f6ab8ac93234f5b4cfd5ed9ddfb151c6e08e0ef

sys: move DRIFT_REMOVAL_INTERVAL definition
In the SunOS and Solaris drivers DRIFT_REMOVAL_INTERVAL needs to be
defined before it's used. This was broken in commit
b6a27df5b9be0f07f151c8fba311cb7eadb2b13e.

sys_solaris.c
@@ -83,6 +83,8 @@ static int index=0;
 /* If 1, need to run dosynctodr().  If 0, don't */
 static int need_dosynctodr = -1;
 
+/* Interval in seconds between adjustments to cancel systematic drift */
+#define DRIFT_REMOVAL_INTERVAL (4.0)
 
 #define GET_ZERO (zeroes[index^=1])
 
@@ -304,9 +306,6 @@ immediate_step(void)
 }
 
 /* ================================================== */
-
-/* Interval in seconds between adjustments to cancel systematic drift */
-#define DRIFT_REMOVAL_INTERVAL (4.0)
 
 static int drift_removal_running = 0;
 static SCH_TimeoutID drift_removal_id;

sys_sunos.c
@@ -76,6 +76,9 @@ static double adjustment_requested;
    */
 static unsigned long our_tickadj = 100;
 
+/* Interval in seconds between adjustments to cancel systematic drift */
+#define DRIFT_REMOVAL_INTERVAL (4.0)
+
 /* ================================================== */
 
 static void
@@ -290,9 +293,6 @@ immediate_step(void)
 }
 
 /* ================================================== */
-
-/* Interval in seconds between adjustments to cancel systematic drift */
-#define DRIFT_REMOVAL_INTERVAL (4.0)
 
 static int drift_removal_running = 0;
 static SCH_TimeoutID drift_removal_id;