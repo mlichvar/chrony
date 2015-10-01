339677806158a18342328d740ac802db64277e32
Miroslav Lichvar <mlichvar@redhat.com>
2015-10-01 14:23:32 +0200

    update copyright years

--- sys_netbsd.c
+++ sys_netbsd.c
@@ -4,6 +4,7 @@
  **********************************************************************
  * Copyright (C) Richard P. Curnow  1997-2001
  * Copyright (C) J. Hannken-Illjes  2001
+ * Copyright (C) Miroslav Lichvar  2015
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of version 2 of the GNU General Public License as
