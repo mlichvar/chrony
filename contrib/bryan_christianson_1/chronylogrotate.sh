024842a38b9cff743de8101429f1c8e693070730
Bryan Christianson <bryan@whatroute.net>
2015-11-30 10:13:03 +1300

    contrib: update chronylogrotate.sh script
    1. Remove obsolete options when running chronyc
    2. Add copyright/licence notice
    3. Use logger utility to print/store error messages

--- contrib/bryan_christianson_1/chronylogrotate.sh
+++ contrib/bryan_christianson_1/chronylogrotate.sh
@@ -1,19 +1,26 @@
 #!/bin/sh
 
-#  chronylogrotate.sh
-#  ChronyControl
+#  chronyd/chronyc - Programs for keeping computer clocks accurate.
 #
-#  Created by Bryan Christianson on 12/07/15.
-#
+#  **********************************************************************
+#  * Copyright (C) Bryan Christianson  2015
+#  *
+#  * This program is free software; you can redistribute it and/or modify
+#  * it under the terms of version 2 of the GNU General Public License as
+#  * published by the Free Software Foundation.
+#  *
+#  * This program is distributed in the hope that it will be useful, but
+#  * WITHOUT ANY WARRANTY; without even the implied warranty of
+#  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  * General Public License for more details.
+#  *
+#  * You should have received a copy of the GNU General Public License along
+#  * with this program; if not, write to the Free Software Foundation, Inc.,
+#  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#  *
+#  **********************************************************************
 
 LOGDIR=/var/log/chrony
-
-if [ ! -e "$LOGDIR" ]; then
-  echo "missing directory: $LOGDIR"
-  exit 1
-fi
-
-cd $LOGDIR
 
 rotate () {
   prefix=$1
@@ -33,13 +40,19 @@ rotate () {
   fi
 }
 
+if [ ! -e "$LOGDIR" ]; then
+  logger -s "missing directory: $LOGDIR"
+  exit 1
+fi
+
+cd $LOGDIR
+
 rotate measurements
 rotate statistics
 rotate tracking
 
 #
 # signal chronyd via chronyc
-
-/usr/local/bin/chronyc -a -f /etc/chrony.d/chrony.conf cyclelogs > /dev/null
+/usr/local/bin/chronyc cyclelogs > /dev/null
 
 exit $?
