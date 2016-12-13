mlichvar/chrony

commit 967e358dbc93aa7a99c3c0ee2a634f1fedecba74

stubs: don't call DNS_Name2IPAddress handler directly
Instead of calling the handler directly schedule a timeout with zero
delay for resolving to make the function behave similarly to the real
asynchronous resolver. This should prevent problems with code that
inadvertently depends on this behavior and which would break only when
compiled without support for asynchronous resolving.

stubs.c
@@ -32,12 +32,14 @@
 #include "keys.h"
 #include "logging.h"
 #include "manual.h"
+#include "memory.h"
 #include "nameserv.h"
 #include "nameserv_async.h"
 #include "ntp_core.h"
 #include "ntp_io.h"
 #include "ntp_sources.h"
 #include "refclock.h"
+#include "sched.h"
 
 #ifndef FEAT_ASYNCDNS
 
@@ -45,20 +47,43 @@
 
 /* This is a blocking implementation used when asynchronous resolving is not available */
 
-void
-DNS_Name2IPAddressAsync(const char *name, DNS_NameResolveHandler handler, void *anything)
+struct DNS_Async_Instance {
+  const char *name;
+  DNS_NameResolveHandler handler;
+  void *arg;
+};
+
+static void
+resolve_name(void *anything)
 {
+  struct DNS_Async_Instance *inst;
   IPAddr addrs[MAX_ADDRESSES];
   DNS_Status status;
   int i;
 
-  status = DNS_Name2IPAddress(name, addrs, MAX_ADDRESSES);
+  inst = (struct DNS_Async_Instance *)anything;
+  status = DNS_Name2IPAddress(inst->name, addrs, MAX_ADDRESSES);
 
   for (i = 0; status == DNS_Success && i < MAX_ADDRESSES &&
-              addrs[i].family != IPADDR_UNSPEC; i++)
+       addrs[i].family != IPADDR_UNSPEC; i++)
     ;
 
-  (handler)(status, i, addrs, anything);
+  (inst->handler)(status, i, addrs, inst->arg);
+
+  Free(inst);
+}
+
+void
+DNS_Name2IPAddressAsync(const char *name, DNS_NameResolveHandler handler, void *anything)
+{
+  struct DNS_Async_Instance *inst;
+
+  inst = MallocNew(struct DNS_Async_Instance);
+  inst->name = name;
+  inst->handler = handler;
+  inst->arg = anything;
+
+  SCH_AddTimeoutByDelay(0.0, resolve_name, inst);
 }
 
 #endif /* !FEAT_ASYNCDNS */

nameserv_async.h
@@ -34,8 +34,7 @@
 typedef void (*DNS_NameResolveHandler)(DNS_Status status, int n_addrs, IPAddr *ip_addrs, void *anything);
 
 /* Request resolving of a name to IP address. The handler will be
-   called when the result is available, but it may be also called
-   directly from this function call. */
+   called when the result is available. */
 extern void DNS_Name2IPAddressAsync(const char *name, DNS_NameResolveHandler handler, void *anything);
 
 #endif

commit 0c9a19ded56ed893aab02e1f62ca259236bdf6e0

stubs: rework emulation of asynchronous resolver to use pipes
With a larger number of configured servers, the handler of the emulated
resolver repeatedly scheduled timeout of zero, which triggered the
infinite loop detection in the scheduler and caused abort. This bug was
introduced in commit 967e358dbc93aa7a99c3c0ee2a634f1fedecba74.

Rework the code to use pipes instead of timeouts to avoid this problem.

stubs.c
@@ -42,6 +42,7 @@
 #include "privops.h"
 #include "refclock.h"
 #include "sched.h"
+#include "util.h"
 
 #ifndef FEAT_ASYNCDNS
 
@@ -51,10 +52,11 @@ struct DNS_Async_Instance {
   const char *name;
   DNS_NameResolveHandler handler;
   void *arg;
+  int pipe[2];
 };
 
 static void
-resolve_name(void *anything)
+resolve_name(int fd, int event, void *anything)
 {
   struct DNS_Async_Instance *inst;
   IPAddr addrs[DNS_MAX_ADDRESSES];
@@ -62,6 +64,11 @@ resolve_name(void *anything)
   int i;
 
   inst = (struct DNS_Async_Instance *)anything;
+
+  SCH_RemoveFileHandler(inst->pipe[0]);
+  close(inst->pipe[0]);
+  close(inst->pipe[1]);
+
   status = PRV_Name2IPAddress(inst->name, addrs, DNS_MAX_ADDRESSES);
 
   for (i = 0; status == DNS_Success && i < DNS_MAX_ADDRESSES &&
@@ -83,7 +90,16 @@ DNS_Name2IPAddressAsync(const char *name, DNS_NameResolveHandler handler, void *
   inst->handler = handler;
   inst->arg = anything;
 
-  SCH_AddTimeoutByDelay(0.0, resolve_name, inst);
+  if (pipe(inst->pipe))
+    LOG_FATAL(LOGF_Nameserv, "pipe() failed");
+
+  UTI_FdSetCloexec(inst->pipe[0]);
+  UTI_FdSetCloexec(inst->pipe[1]);
+
+  SCH_AddFileHandler(inst->pipe[0], SCH_FILE_INPUT, resolve_name, inst);
+
+  if (write(inst->pipe[1], "", 1) < 0)
+    ;
 }
 
 #endif /* !FEAT_ASYNCDNS */