5aae563277208816d0b7da5df8b48522a7b3aacf
Miroslav Lichvar <mlichvar@redhat.com>
2018-03-22 18:05:00 +0100

    update copyright years

--- client.c
+++ client.c
@@ -4,7 +4,7 @@
  **********************************************************************
  * Copyright (C) Richard P. Curnow  1997-2003
  * Copyright (C) Lonnie Abelbeck  2016
- * Copyright (C) Miroslav Lichvar  2009-2017
+ * Copyright (C) Miroslav Lichvar  2009-2018
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of version 2 of the GNU General Public License as
