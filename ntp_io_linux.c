9220c9b8a235252eb365da25973cfc1ad09c85ed
Miroslav Lichvar <mlichvar@redhat.com>
2019-05-09 16:55:04 +0200

    update copyright years

--- ntp_io_linux.c
+++ ntp_io_linux.c
@@ -2,7 +2,7 @@
   chronyd/chronyc - Programs for keeping computer clocks accurate.
 
  **********************************************************************
- * Copyright (C) Miroslav Lichvar  2016-2018
+ * Copyright (C) Miroslav Lichvar  2016-2019
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of version 2 of the GNU General Public License as
