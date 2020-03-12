mlichvar/chrony

commit 049eae661ae350c242a97109b8672a2c36558038

sources: keep synchronized status with unreachable/unselectable sources
Following RFC 5905, don't call REF_SetUnsynchronised() when there are no
reachable or selectable sources. It's up to the client to consider the
source unsynchronized when the root distance exceeds a threshold.

The unsynchronized status is still set when no majority is reached.

sources.c
@@ -571,7 +571,7 @@ combine_sources(int n_sel_sources, struct timeval *ref_time, double *offset,
 void
 SRC_SelectSource(SRC_Instance updated_inst)
 {
-  int i, j, index, old_selected_index, sel_prefer;
+  int i, j, index, sel_prefer;
   struct timeval now, ref_time;
   double src_offset, src_offset_sd, src_frequency, src_skew;
   double src_root_delay, src_root_dispersion;
@@ -588,7 +588,6 @@ SRC_SelectSource(SRC_Instance updated_inst)
   double max_score;
 
   NTP_Leap leap_status = LEAP_Normal;
-  old_selected_index = selected_source_index;
 
   if (updated_inst)
     updated_inst->updates++;
@@ -598,7 +597,6 @@ SRC_SelectSource(SRC_Instance updated_inst)
     if (selected_source_index != INVALID_SOURCE) {
       log_selection_message("Can't synchronise: no sources", NULL);
       selected_source_index = INVALID_SOURCE;
-      REF_SetUnsynchronised();
     }
     return;
   }
@@ -748,6 +746,7 @@ SRC_SelectSource(SRC_Instance updated_inst)
 
       if (selected_source_index != INVALID_SOURCE) {
         log_selection_message("Can't synchronise: no majority", NULL);
+        REF_SetUnsynchronised();
       }
       selected_source_index = INVALID_SOURCE;
 
@@ -989,11 +988,6 @@ SRC_SelectSource(SRC_Instance updated_inst)
       log_selection_message("Can't synchronise: no reachable sources", NULL);
     }
     selected_source_index = INVALID_SOURCE;
-  }
-
-  if (selected_source_index == INVALID_SOURCE &&
-      selected_source_index != old_selected_index) {
-    REF_SetUnsynchronised();
   }
 }
 

commit 18d7ea62b334cb852cd6269e11444f9f39379d38

reference: don't report synchronized status after unknown step
The source handler resets SST instances on an unknown step, which
makes the sources unselectable, but SRC_SelectSource() doesn't call
REF_SetUnsynchronised() when no source is selectable.

Handle the step in the reference handler.

Fixes: 049eae661ae3 ("sources: keep synchronized status with unreachable/unselectable sources")

reference.c
@@ -168,6 +168,7 @@ handle_slew(struct timespec *raw,
 
   if (change_type == LCL_ChangeUnknownStep) {
     last_ref_update = 0.0;
+    REF_SetUnsynchronised();
   }
 
   /* When the clock was stepped, check if that doesn't change our leap status

sources.c
@@ -1128,7 +1128,7 @@ slew_sources(struct timespec *raw, struct timespec *cooked, double dfreq,
   }
 
   if (change_type == LCL_ChangeUnknownStep) {
-    /* After resetting no source is selectable, set reference unsynchronised */
+    /* Update selection status */
     SRC_SelectSource(NULL);
   }
 }