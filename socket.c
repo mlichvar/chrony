mlichvar/chrony

commit f231efb811eef90a53876f801d7e2aaa2a60574f

socket: add support for sending and receiving descriptors
Add support for the SCM_RIGHTS control message used in communication
with the privops helper.

socket.c
@@ -630,12 +630,15 @@ init_message_nonaddress(SCK_Message *message)
   message->timestamp.if_index = INVALID_IF_INDEX;
   message->timestamp.l2_length = 0;
   message->timestamp.tx_flags = 0;
+
+  message->descriptor = INVALID_SOCK_FD;
 }
 
 /* ================================================== */
 
 static int
-process_header(struct msghdr *msg, unsigned int msg_length, int sock_fd, SCK_Message *message)
+process_header(struct msghdr *msg, unsigned int msg_length, int sock_fd, int flags,
+               SCK_Message *message)
 {
   struct cmsghdr *cmsg;
 
@@ -773,6 +776,18 @@ process_header(struct msghdr *msg, unsigned int msg_length, int sock_fd, SCK_Mes
       }
     }
 #endif
+
+    if (cmsg->cmsg_level == SOL_SOCKET && cmsg->cmsg_type == SCM_RIGHTS) {
+      if (!(flags & SCK_FLAG_MSG_DESCRIPTOR) || cmsg->cmsg_len != CMSG_LEN(sizeof (int))) {
+        unsigned int i;
+
+        DEBUG_LOG("Unexpected SCM_RIGHTS");
+        for (i = 0; CMSG_LEN((i + 1) * sizeof (int)) <= cmsg->cmsg_len; i++)
+          close(((int *)CMSG_DATA(cmsg))[i]);
+        return 0;
+      }
+      message->descriptor = *(int *)CMSG_DATA(cmsg);
+    }
   }
 
   return 1;
@@ -823,7 +838,7 @@ receive_messages(int sock_fd, SCK_Message *messages, int max_messages, int flags
 
   for (i = 0; i < n; i++) {
     hdr = ARR_GetElement(recv_headers, i);
-    if (!process_header(&hdr->msg_hdr, hdr->msg_len, sock_fd, &messages[i]))
+    if (!process_header(&hdr->msg_hdr, hdr->msg_len, sock_fd, flags, &messages[i]))
       return 0;
 
     log_message(sock_fd, 1, &messages[i],
@@ -875,8 +890,6 @@ send_message(int sock_fd, SCK_Message *message, int flags)
   socklen_t saddr_len;
   struct msghdr msg;
   struct iovec iov;
-
-  assert(flags == 0);
 
   switch (message->addr_type) {
     case SCK_ADDR_UNSPEC:
@@ -973,6 +986,16 @@ send_message(int sock_fd, SCK_Message *message, int flags)
     *ts_tx_flags = message->timestamp.tx_flags;
   }
 #endif
+
+  if (flags & SCK_FLAG_MSG_DESCRIPTOR) {
+    int *fd;
+
+    fd = add_control_message(&msg, SOL_SOCKET, SCM_RIGHTS, sizeof (*fd), sizeof (cmsg_buf));
+    if (!fd)
+      return 0;
+
+    *fd = message->descriptor;
+  }
 
   /* This is apparently required on some systems */
   if (msg.msg_controllen == 0)

socket.h
@@ -38,6 +38,7 @@
 
 /* Flags for receiving and sending messages */
 #define SCK_FLAG_MSG_ERRQUEUE 1
+#define SCK_FLAG_MSG_DESCRIPTOR 2
 
 /* Maximum number of received messages */
 #define SCK_MAX_RECV_MESSAGES 4
@@ -70,6 +71,8 @@ typedef struct {
     int l2_length;
     int tx_flags;
   } timestamp;
+
+  int descriptor;
 } SCK_Message;
 
 /* Initialisation function */

commit 473cb3c9689ad888e8bba6d9042b314341e4c195

socket: always process control messages
Even if a received message will not be returned to the caller (e.g.
because it is truncated), process its control messages to avoid leaking
received descriptors.

Fixes: f231efb811ee ("socket: add support for sending and receiving descriptors")

socket.c
@@ -742,18 +742,10 @@ process_header(struct msghdr *msg, int msg_length, int sock_fd, int flags,
                SCK_Message *message)
 {
   struct cmsghdr *cmsg;
+  int r = 1;
 
-  if (msg->msg_iovlen != 1) {
-    DEBUG_LOG("Unexpected iovlen");
-    return 0;
-  }
-
-  if (msg->msg_namelen > sizeof (union sockaddr_all)) {
-    DEBUG_LOG("Truncated source address");
-    return 0;
-  }
-
-  if (msg->msg_namelen > sizeof (((struct sockaddr *)msg->msg_name)->sa_family)) {
+  if (msg->msg_namelen <= sizeof (union sockaddr_all) &&
+      msg->msg_namelen > sizeof (((struct sockaddr *)msg->msg_name)->sa_family)) {
     switch (((struct sockaddr *)msg->msg_name)->sa_family) {
       case AF_INET:
 #ifdef FEAT_IPV6
@@ -767,26 +759,38 @@ process_header(struct msghdr *msg, int msg_length, int sock_fd, int flags,
         message->remote_addr.path = ((struct sockaddr_un *)msg->msg_name)->sun_path;
         break;
       default:
+        init_message_addresses(message, SCK_ADDR_UNSPEC);
         DEBUG_LOG("Unexpected address");
-        return 0;
+        r = 0;
+        break;
     }
   } else {
     init_message_addresses(message, SCK_ADDR_UNSPEC);
+
+    if (msg->msg_namelen > sizeof (union sockaddr_all)) {
+      DEBUG_LOG("Truncated source address");
+      r = 0;
+    }
   }
 
   init_message_nonaddress(message);
 
-  message->data = msg->msg_iov[0].iov_base;
-  message->length = msg_length;
+  if (msg->msg_iovlen == 1) {
+    message->data = msg->msg_iov[0].iov_base;
+    message->length = msg_length;
+  } else {
+    DEBUG_LOG("Unexpected iovlen");
+    r = 0;
+  }
 
   if (msg->msg_flags & MSG_TRUNC) {
     log_message(sock_fd, 1, message, "Truncated", NULL);
-    return 0;
+    r = 0;
   }
 
   if (msg->msg_flags & MSG_CTRUNC) {
     log_message(sock_fd, 1, message, "Truncated cmsg in", NULL);
-    return 0;
+    r = 0;
   }
 
   for (cmsg = CMSG_FIRSTHDR(msg); cmsg; cmsg = CMSG_NXTHDR(msg, cmsg)) {
@@ -873,7 +877,7 @@ process_header(struct msghdr *msg, int msg_length, int sock_fd, int flags,
       if (err.ee_errno != ENOMSG || err.ee_info != SCM_TSTAMP_SND ||
           err.ee_origin != SO_EE_ORIGIN_TIMESTAMPING) {
         log_message(sock_fd, 1, message, "Unexpected extended error in", NULL);
-        return 0;
+        r = 0;
       }
     }
 #endif
@@ -885,13 +889,17 @@ process_header(struct msghdr *msg, int msg_length, int sock_fd, int flags,
         DEBUG_LOG("Unexpected SCM_RIGHTS");
         for (i = 0; CMSG_LEN((i + 1) * sizeof (int)) <= cmsg->cmsg_len; i++)
           close(((int *)CMSG_DATA(cmsg))[i]);
-        return 0;
+        r = 0;
+      } else {
+        message->descriptor = *(int *)CMSG_DATA(cmsg);
       }
-      message->descriptor = *(int *)CMSG_DATA(cmsg);
     }
   }
 
-  return 1;
+  if (!r && message->descriptor != INVALID_SOCK_FD)
+    close(message->descriptor);
+
+  return r;
 }
 
 /* ================================================== */