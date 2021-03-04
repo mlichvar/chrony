mlichvar/chrony

commit f06c1cfa97f869204483eda9af72bd250e54e831

cmdmon: respond from same address
Enable the destination address of received messages in order to respond
from the same address on multihomed hosts.

cmdmon.c
@@ -165,7 +165,7 @@ open_socket(int family)
         SCK_GetLoopbackIPAddress(family, &local_addr.ip_addr);
       local_addr.port = port;
 
-      sock_fd = SCK_OpenUdpSocket(NULL, &local_addr, 0);
+      sock_fd = SCK_OpenUdpSocket(NULL, &local_addr, SCK_FLAG_RX_DEST_ADDR);
       if (sock_fd < 0) {
         LOG(LOGS_ERR, "Could not open command socket on %s",
             UTI_IPSockAddrToString(&local_addr));

commit 2d39a12f514673dd2ea2001a33e815956e49cba1

cmdmon: fix responding to IPv4 addresses on FreeBSD
On FreeBSD, the source address cannot be specified when sending a
message on a socket bound to a non-any IPv4 address, e.g. in default
configuration 127.0.0.1. In this case, make the address unspecified.

This is similar to commit 6af39d63aa93 ("ntp: don't use IP_SENDSRCADDR
on bound socket").

Fixes: f06c1cfa97f8 ("cmdmon: respond from same address")

cmdmon.c
@@ -62,6 +62,9 @@ static int sock_fdu;
 static int sock_fd4;
 static int sock_fd6;
 
+/* Flag indicating the IPv4 socket is bound to an address */
+static int bound_sock_fd4;
+
 /* Flag indicating whether this module has been initialised or not */
 static int initialised = 0;
 
@@ -179,6 +182,9 @@ open_socket(int family)
         return INVALID_SOCK_FD;
       }
 
+      if (family == IPADDR_INET4)
+        bound_sock_fd4 = local_addr.ip_addr.addr.in4 != INADDR_ANY;
+
       break;
     case IPADDR_UNSPEC:
       local_path = CNF_GetBindCommandPath();
@@ -243,6 +249,8 @@ CAM_Initialise(void)
   do_size_checks();
 
   initialised = 1;
+
+  bound_sock_fd4 = 0;
 
   sock_fdu = INVALID_SOCK_FD;
   sock_fd4 = open_socket(IPADDR_INET4);
@@ -309,6 +317,12 @@ transmit_reply(int sock_fd, int request_length, SCK_Message *message)
   if (message->addr_type == SCK_ADDR_IP &&
       !SCK_IsLinkLocalIPAddress(&message->remote_addr.ip.ip_addr))
     message->if_index = INVALID_IF_INDEX;
+
+#if !defined(HAVE_IN_PKTINFO) && defined(IP_SENDSRCADDR)
+  /* On FreeBSD a local IPv4 address cannot be specified on bound socket */
+  if (message->local_addr.ip.family == IPADDR_INET4 && (sock_fd != sock_fd4 || bound_sock_fd4))
+    message->local_addr.ip.family = IPADDR_UNSPEC;
+#endif
 
   if (!SCK_SendMessage(sock_fd, message, 0))
     return;