bb9ba3e4bd31692674cb0ff1fcfe8ff6c1f05edb
Miroslav Lichvar <mlichvar@redhat.com>
2021-05-04 11:49:54 +0200

    source: don't print duplicated address in selection message
    Don't print the original IP address in parentheses in the "Selected
    source ..." message if it is identical to the current address. That is
    expected to be the usual case for sources specified by IP address.

--- sources.c
+++ sources.c
@@ -589,7 +589,7 @@ log_selection_source(const char *format, SRC_Instance inst)
   name = source_to_string(inst);
   ntp_name = inst->type == SRC_NTP ? NSR_GetName(inst->ip_addr) : NULL;
 
-  if (ntp_name)
+  if (ntp_name && strcmp(name, ntp_name) != 0)
     snprintf(buf, sizeof (buf), "%s (%s)", name, ntp_name);
   else
     snprintf(buf, sizeof (buf), "%s", name);
