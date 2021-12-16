55f48b14b720a7cfcfb0687bc50f0f6e36c95964
Miroslav Lichvar <mlichvar@redhat.com>
2021-12-16 09:54:11 +0100

    update copyright years

--- test/unit/clientlog.c
+++ test/unit/clientlog.c
@@ -1,6 +1,6 @@
 /*
  **********************************************************************
- * Copyright (C) Miroslav Lichvar  2016
+ * Copyright (C) Miroslav Lichvar  2016, 2021
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of version 2 of the GNU General Public License as
