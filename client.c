7b197953e8add5515b7e58c4638dc55aa4bb91b7
Miroslav Lichvar <mlichvar@redhat.com>
2022-08-29 15:04:33 +0200

    update copyright years

--- client.c
+++ client.c
@@ -3300,7 +3300,7 @@ static void
 display_gpl(void)
 {
     printf("chrony version %s\n"
-           "Copyright (C) 1997-2003, 2007, 2009-2021 Richard P. Curnow and others\n"
+           "Copyright (C) 1997-2003, 2007, 2009-2022 Richard P. Curnow and others\n"
            "chrony comes with ABSOLUTELY NO WARRANTY.  This is free software, and\n"
            "you are welcome to redistribute it under certain conditions.  See the\n"
            "GNU General Public License version 2 for details.\n\n",
