b9f527884638a336f3fd3b015d85a7d7155b0062
Miroslav Lichvar <mlichvar@redhat.com>
2024-10-07 16:17:07 +0200

    update copyright years

--- test/unit/nts_ke_client.c
+++ test/unit/nts_ke_client.c
@@ -1,6 +1,6 @@
 /*
  **********************************************************************
- * Copyright (C) Miroslav Lichvar  2020
+ * Copyright (C) Miroslav Lichvar  2020, 2024
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of version 2 of the GNU General Public License as
